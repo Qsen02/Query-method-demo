1b4e31950a4f9d68f7988c80eea59ef72e5a1997
Ясен Генчев <101555544+Qsen02@users.noreply.github.com>
2026-09-13 11:23:02 +0300

    Router implemented

---
+++ Server/src/config/router.js
@@ -0,0 +1,27 @@
+const { getAllNotes } = require("../services/Notes.js");
+
+function routerConfig(app) {
+	app.get("/notes", async (req, res) => {
+		const notes = await getAllNotes();
+		res.json(notes);
+    });
+
+    app.post("/notes", async (req, res) => { 
+        const fields = req.body;
+        const newNote = await createNote(fields.title, fields.content);
+        res.json(newNote);
+    });
+    
+    app.use("/notes/search", async (req, res) => {
+        if (req.method !== "QUERY") { 
+            return res.status(405).json({ error: "Method not allowed" });
+        }
+        const query = req.body;
+        const notes = await searchNotes(query.criteria, query.value);
+        res.json(notes);
+    });
+}
+
+module.exports = {
+    routerConfig,
+}
--- Server/src/index.js
+++ Server/src/index.js
@@ -2,6 +2,7 @@ const Express = require('express');
 const dotenv = require('dotenv');
 const { expressConfig } = require('./config/express');
 const { runDB } = require('./config/mongoose');
+const { routerConfig } = require('./config/router');
 
 dotenv.config();
 const app = Express();
@@ -10,6 +11,7 @@ const port = process.env.PORT || 3000;
 async function startServer() {
     await runDB();
     expressConfig(app);
+    routerConfig(app);
 
     app.listen(port, () => { 
         console.log(`Server is running on port ${port}`);
